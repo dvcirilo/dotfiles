d64f85e7f1bbed129905053d94a3ab75b1f85700
Diego Vinicius <eletronzip@gmail.com>
2011-09-30 03:35:48 -0300

    script to install packages i normally use

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#basic stuff that I use
+sudo apt-get install git vim
+
+#necessary for RVM
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison curl ruby ruby-dev
+
+#necessary for web navigation
+sudo apt-get install flashplugin-installer ttf-mscorefonts-installer
