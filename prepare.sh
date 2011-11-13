51933ac6b235cc2e497795f5de8259e8a1ffcf19
Diego Vinicius <eletronzip@gmail.com>
2011-11-13 04:02:53 -0300

    prepare things

--- prepare.sh
+++ prepare.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+#Configuring cedilla
+# http://gpupo.com/c-cedilha-no-ubuntu-910-resolvido
+
 #basic stuff that I use
 sudo apt-get install git vim
 
 #necessary for RVM
 sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison curl ruby ruby-dev
 
-#necessary for web navigation
-sudo apt-get install flashplugin-installer ttf-mscorefonts-installer
+#necessary for almost anything
+sudo apt-get install ubuntu-restricted-extras
 
 #necessary for akita's vimfiles.
 sudo apt-get install exuberant-ctags ncurses-term
 
 #necessary for mysql db
-sudo apt-get install libmysqlclient-dev mysql-server mysql-client mysql-common
+sudo apt-get install libmysqlclient-dev mysql-server mysql-client mysql-common mysql-admin
 
-#audacious 2.5.0
+#LAMP
+sudo apt-get install apache2 libapache2-mod-php5 libapache2-mod-auth-mysql php5-mysql
+
+#audacious 2.5.x
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get install audacious
