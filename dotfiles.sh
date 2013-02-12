7895c7be34854994d45dc556cc6d468a6207d8f0
Diego Cirilo <eletronzip@gmail.com>
2013-02-12 05:58:25 -0300

    better dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,16 +2,19 @@
 
 #My face on gnome
 echo "Getting my face from gravatar.com and putting it into .face"
-wget "http://gravatar.com/avatar/4e2372887c285333cf7be7901de22bb3?s=200" -O .face
+wget "http://gravatar.com/avatar/4e2372887c285333cf7be7901de22bb3?s=200" -O ~/.face
 
 #ZSH config
+rm -rf ~/.oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
-echo "source ~/.dotfiles/zshrc" > ~/.zshrc
+echo "source ~/.dotfiles/zshrc" ~/.zshrc
 cp ~/.dotfiles/halan.zsh-theme ~/.oh-my-zsh/themes/
+chsh -s /bin/zsh
 
 #vim config
 echo "Configuring VIM with my own dotfiles"
-git clone git://github.com/eletronzip/vimfiles.git > ~/.vim
+rm -rf ~/.vim
+git clone git://github.com/eletronzip/vimfiles.git  ~/.vim
 cd ~/.vim/
 git submodule update --init
 cd ~
@@ -23,7 +26,9 @@ echo "source ~/.dotfiles/bashrc" > ~/.bashrc
 echo "source ~/.dotfiles/bashrc" > ~/.bash_profile
 
 #Gitconfig
+rm -f ~/.gitconfig
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 
 #Gemrc
+rm -f ~/.gemrc
 ln -s ~/.dotfiles/gemrc ~/.gemrc
